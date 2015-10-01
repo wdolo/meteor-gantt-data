b0317a7242242dffd47b296f11c836429db9a289
Will Dolatowski <dolatow1@gmail.com>
2015-10-01 12:26:47 -0500

    gantt printing

--- gantt-data.js
+++ gantt-data.js
@@ -34,7 +34,7 @@ function meteorStart(collections) {
 
     gantt.parse(collection);
 
-    initCollectionHandler(this, collections["tasks"], collectionsCursors["tasks"], "task");
+    initCollectionHandler(this, collections["tasks"], collectionsCursors["tasks"], "task", collections["links"]);
     initCollectionHandler(this, collections["links"], collectionsCursors["links"], "link");
 
 }
@@ -57,15 +57,11 @@ function meteorStop() {
         });
         gEventsCollection.clean();
     }
-    //gantt.detachEvent("onTaskLoading");
-    //gantt.detachEvent("onAfterTaskAdd");
-    //gantt.detachEvent("onAfterTaskUpdate");
-    //gantt.detachEvent("onAfterTaskDelete");
     gantt.clearAll();
 
 }
 
-function initCollectionHandler(gantt, collection, collectionCursor, itemType) {
+function initCollectionHandler(gantt, collection, collectionCursor, itemType, linksCollection) {
     var itemTypeSettings = getItemTypeSettings(gantt, itemType),
         eventsNames = itemTypeSettings.events_names;
 
@@ -83,13 +79,95 @@ function initCollectionHandler(gantt, collection, collectionCursor, itemType) {
 
     gEventsCollection.add(gantt.attachEvent(eventsNames.updated, function(itemId, item) {
         console.log('updating');
+
         collectionHandlerObj.save(item);
     }));
 
     gEventsCollection.add(gantt.attachEvent(eventsNames.removed, function(itemId, item) {
         console.log('removing');
+        console.log(itemId);
+        console.log(item);
         collectionHandlerObj.remove(itemId, item);
     }));
+
+
+    if (linksCollection) {
+        gEventsCollection.add(gantt.attachEvent("onAfterTaskDrag", function (id, e) {
+            console.log('done dragging');
+            var links, task;
+
+            links = linksCollection.find({$or:[{source:id},{target:id}]}).fetch();
+            links.forEach(function (link) {
+                if (!link.linkType || link.linkType === "hard"){
+                    if (link.source === id && link.target) {
+                        task = gantt.getTask(link.target);
+                        gantt.roundTaskDates(task);
+                        gantt.updateTask(link.target);
+                    } else if (link.target === id && link.source) {
+                        task = gantt.getTask(link.source);
+                        gantt.roundTaskDates(task);
+                        gantt.updateTask(link.source);
+                    }
+                }
+
+            });
+        }));
+
+        gEventsCollection.add(gantt.attachEvent("onTaskDrag", function (id, mode, copy, original, e) {
+            var dependencies,
+                links, obj,
+                diff = new Date(original.start_date).getTime() - new Date(copy.start_date).getTime();
+            if (mode === "move") {
+                links = linksCollection.find({$or:[{source:copy._id},{target:copy._id}]}).fetch();
+                links.forEach(function (link) {
+                    if (link && link.linkType && link.linkType === "hard") {
+                        console.log(copy);
+                        console.log(link);
+                        if (link.source === copy._id && link.target) {
+                            console.log('yooo');
+                            console.log(link.target);
+                            dependencies = gantt.getTask(link.target.toString());
+                            if (dependencies && dependencies.start_date) {
+                                dependencies.start_date = new Date(new Date(dependencies.start_date).getTime() - diff);
+                                dependencies.end_date = new Date(new Date(dependencies.end_date).getTime() - diff);
+
+                                gantt.updateTask(link.target);
+                                //gantt.roundTaskDates(dependencies);
+                            }
+                        } else if (link.target === copy._id && link.source){
+                            dependencies = gantt.getTask(link.source.toString());
+                            if (dependencies && dependencies.start_date) {
+                                dependencies.start_date = new Date(new Date(dependencies.start_date).getTime() - diff);
+                                dependencies.end_date = new Date(new Date(dependencies.end_date).getTime() - diff);
+                                console.log('yo');
+                                gantt.updateTask(link.source);
+                                //gantt.roundTaskDates(dependencies);
+                            }
+                        }
+                    }
+                });
+            }
+        }));
+    } else {
+        gEventsCollection.add(gantt.attachEvent("onLinkClick", function (id, e) {
+            console.log(e);
+            var link = gantt.getLink(id),
+                linkType;
+            if (link.linkType && link.linkType === "hard"){
+                linkType = "soft";
+                link.linkType = linkType;
+            } else {
+                linkType = "hard";
+                link.linkType = linkType;
+            }
+            collection.update({_id:link._id}, {$set:{
+                linkType: linkType
+            }});
+
+            gantt.updateLink(id)
+
+        }));
+    }
 
     var methods = itemTypeSettings.methods;
     gObserversCollection.add(collectionCursor.observe({
@@ -113,6 +191,8 @@ function initCollectionHandler(gantt, collection, collectionCursor, itemType) {
             for(var key in itemData)
                 item[key] = itemData[key];
 
+            console.log('yo');
+            console.log(itemData);
             methods.update(itemData._id);
             return true;
         },
@@ -187,6 +267,7 @@ function CollectionHandler(collection) {
 
         if(savedItemData){
             if (item.hasOwnProperty("text")) {
+                console.log(savedItemData);
                 collection.update({_id: savedItemData._id}, {
                     $set: {
                         text: item.text,
@@ -196,6 +277,7 @@ function CollectionHandler(collection) {
                     }
                 });
             } else {
+                console.log(item);
                 collection.update({_id: savedItemData._id}, {
                     $set: {
                         source: item.source,
@@ -205,6 +287,9 @@ function CollectionHandler(collection) {
             }
         }
         else {
+            if (item.source) {
+                item.linkType = "hard";
+            }
             console.log(item);
             console.log('insertting');
             collection.insert(item);
@@ -212,9 +297,12 @@ function CollectionHandler(collection) {
     };
 
     this.remove = function(itemId, item) {
-        console.log(item);
-        if (item)
+        if (item && item._id) {
             collection.remove({_id:item._id});
+        } else {
+            var Item = collection.findOne({source:item.source, target:item.target});
+            collection.remove({_id:Item._id});
+        }
     };
 
     this.findItem = function(itemId) {
